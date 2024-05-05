6a9dc8c25b9523c4749c54c748d1944c69de4645
tinytransfem <n3rilp@gmail.com>
2024-05-05 14:06:31 +0200

    made mod not load on server

--- fabric/src/main/java/org/infernalstudios/celesteconfig/CelestialConfiguration.java
+++ fabric/src/main/java/org/infernalstudios/celesteconfig/CelestialConfiguration.java
@@ -2,13 +2,14 @@ package org.infernalstudios.celesteconfig;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import org.infernalstudios.celesteconfig.config.CelestialConfigClothConfig;
 
-public class CelestialConfiguration implements ModInitializer {
+public class CelestialConfiguration implements ClientModInitializer {
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         AutoConfig.register(CelestialConfigClothConfig.class, Toml4jConfigSerializer::new);
         CelestialConfigClothConfig config = AutoConfig.getConfigHolder(CelestialConfigClothConfig.class).getConfig();
 
