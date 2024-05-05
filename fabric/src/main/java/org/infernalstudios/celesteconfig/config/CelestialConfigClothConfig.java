a7370a244ec7caf9a1bd2e1c1092ca87e3d165a8
tinytransfem <n3rilp@gmail.com>
2024-05-05 08:18:37 +0200

    Initial port to MultiLoader 1.20.1

---
+++ fabric/src/main/java/org/infernalstudios/celesteconfig/config/CelestialConfigClothConfig.java
@@ -0,0 +1,12 @@
+package org.infernalstudios.celesteconfig.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = "celesteconfig")
+public class CelestialConfigClothConfig implements ConfigData {
+    public double moonWidth = 10.0D;
+    public double moonHeight = 1.0D;
+    public double sunWidth = 1.0D;
+    public double sunHeight = 10.0D;
+}
