3efced1c1f359aba34dc765c451dab648c37b162
tinytransfem <n3rilp@gmail.com>
2024-05-05 14:05:56 +0200

    fixed default config values

--- fabric/src/main/java/org/infernalstudios/celesteconfig/config/CelestialConfigClothConfig.java
+++ fabric/src/main/java/org/infernalstudios/celesteconfig/config/CelestialConfigClothConfig.java
@@ -5,8 +5,8 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = "celesteconfig")
 public class CelestialConfigClothConfig implements ConfigData {
-    public double moonWidth = 10.0D;
+    public double moonWidth = 1.0D;
     public double moonHeight = 1.0D;
     public double sunWidth = 1.0D;
-    public double sunHeight = 10.0D;
+    public double sunHeight = 1.0D;
 }
