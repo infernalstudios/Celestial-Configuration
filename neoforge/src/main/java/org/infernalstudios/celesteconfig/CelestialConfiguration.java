ad36fdabcb2cb90eadc5aa4a358ae887b67b29ad
Ciri <n3rilp@gmail.com>
2024-07-05 16:29:31 +0200

    initial 1.21 port

--- neoforge/src/main/java/org/infernalstudios/celesteconfig/CelestialConfiguration.java
+++ neoforge/src/main/java/org/infernalstudios/celesteconfig/CelestialConfiguration.java
@@ -1,13 +1,18 @@
 package org.infernalstudios.celesteconfig;
 
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.ModConfigSpec;
 import org.infernalstudios.celesteconfig.config.CelestialConfigOptions;
 
 @Mod(Constants.MOD_ID)
 public class CelestialConfiguration {
+
+    public static ModConfigSpec CLIENT_CONFIG;
     
-    public CelestialConfiguration() {
-        CelestialConfigOptions.init();
+    public CelestialConfiguration(ModContainer modContainer) {
+        CelestialConfigOptions.init(modContainer);
         CommonClass.init(CelestialConfigOptions.getMoonWidthScalar(), CelestialConfigOptions.getMoonHeightScalar(), CelestialConfigOptions.getSunWidthScalar(), CelestialConfigOptions.getSunHeightScalar());
     }
 }
--- neoforge/src/main/java/org/infernalstudios/celesteconfig/config/CelestialConfigOptions.java
+++ neoforge/src/main/java/org/infernalstudios/celesteconfig/config/CelestialConfigOptions.java
@@ -2,7 +2,7 @@ package org.infernalstudios.celesteconfig.config;
 
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
-import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.common.ModConfigSpec;
@@ -16,21 +16,21 @@ public class CelestialConfigOptions {
     private static ModConfigSpec.DoubleValue moonHeightScalar;
     private static ModConfigSpec.DoubleValue sunWidthScalar;
     private static ModConfigSpec.DoubleValue sunHeightScalar;
-    private static ModConfigSpec client_config;
+    private static ModConfigSpec CLIENT_CONFIG;
 
-    public static void init() {
+    public static void init(ModContainer modContainer) {
         client_builder.comment("Celestial Configuration");
         moonWidthScalar = client_builder.comment("Moon Width Scalar").defineInRange("celesteconfig.moon.width", 1.0D, 0.0D, 100.0D);
         moonHeightScalar = client_builder.comment("Moon Height Scalar").defineInRange("celesteconfig.moon.height", 1.0D, 0.0D, 100.0D);
         sunWidthScalar = client_builder.comment("Sun Width Scalar").defineInRange("celesteconfig.sun.width", 1.0D, 0.0D, 100.0D);
         sunHeightScalar = client_builder.comment("Moon Height Scalar").defineInRange("celesteconfig.sun.height", 1.0D, 0.0D, 100.0D);
-        client_config = client_builder.build();
+        CLIENT_CONFIG = client_builder.build();
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CelestialConfigOptions.client_config);
+        modContainer.registerConfig(ModConfig.Type.CLIENT, CLIENT_CONFIG);
 
         CommentedFileConfig file = CommentedFileConfig.builder(new File(FMLPaths.CONFIGDIR.get().resolve("celesteconfig-client.toml").toString())).sync().autosave().writingMode(WritingMode.REPLACE).build();
         file.load();
-        client_config.setConfig(file);
+        CLIENT_CONFIG.setConfig(file);
     }
 
     public static double getMoonWidthScalar() {
