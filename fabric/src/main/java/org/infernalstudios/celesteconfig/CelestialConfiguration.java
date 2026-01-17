a387534d12de1680d6c1bdc0c7431c657509bdd4
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-17 13:24:09 -0800

    Live reloading

--- fabric/src/main/java/org/infernalstudios/celesteconfig/CelestialConfiguration.java
+++ fabric/src/main/java/org/infernalstudios/celesteconfig/CelestialConfiguration.java
@@ -3,6 +3,7 @@ package org.infernalstudios.celesteconfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.world.InteractionResult;
 import org.infernalstudios.celesteconfig.config.CelestialConfigClothConfig;
 
 public class CelestialConfiguration implements ClientModInitializer {
@@ -10,8 +11,13 @@ public class CelestialConfiguration implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         AutoConfig.register(CelestialConfigClothConfig.class, Toml4jConfigSerializer::new);
-        CelestialConfigClothConfig config = AutoConfig.getConfigHolder(CelestialConfigClothConfig.class).getConfig();
 
+        AutoConfig.getConfigHolder(CelestialConfigClothConfig.class).registerSaveListener((holder, config) -> {
+            CommonClass.init(config.moonWidth, config.moonHeight, config.sunWidth, config.sunHeight);
+            return InteractionResult.PASS;
+        });
+
+        CelestialConfigClothConfig config = AutoConfig.getConfigHolder(CelestialConfigClothConfig.class).getConfig();
         CommonClass.init(config.moonWidth, config.moonHeight, config.sunWidth, config.sunHeight);
     }
 }
--- forge/src/main/java/org/infernalstudios/celesteconfig/CelestialConfigration.java
+++ forge/src/main/java/org/infernalstudios/celesteconfig/CelestialConfigration.java
@@ -1,13 +1,20 @@
 package org.infernalstudios.celesteconfig;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.infernalstudios.celesteconfig.config.CelestialConfigOptions;
 
 @Mod(Constants.MOD_ID)
 public class CelestialConfigration {
-    
+
     public CelestialConfigration() {
         CelestialConfigOptions.init();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onConfigLoad);
+        CommonClass.init(CelestialConfigOptions.getMoonWidthScalar(), CelestialConfigOptions.getMoonHeightScalar(), CelestialConfigOptions.getSunWidthScalar(), CelestialConfigOptions.getSunHeightScalar());
+    }
+
+    public void onConfigLoad(ModConfigEvent event) {
         CommonClass.init(CelestialConfigOptions.getMoonWidthScalar(), CelestialConfigOptions.getMoonHeightScalar(), CelestialConfigOptions.getSunWidthScalar(), CelestialConfigOptions.getSunHeightScalar());
     }
 }
